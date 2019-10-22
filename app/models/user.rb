c374696e430b4a688be9da0107032cec9ac6cbe6
Omeshwor Shrestha <omeshworlalshrestha@yahoo.com>
2019-10-21 23:00:39 -0500

    users and sessions

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  before_save :downcase_email
+
+  validates :first_name, presence: true, length: {minimum: 2, maximum: 50}
+  validates :last_name, presence: true, length: {minimum: 2, maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, length: {minimum: 6}, presence: true
+  has_secure_password
+
+  private
+  def downcase_email
+    self.email = email.downcase
+  end
+
+end
